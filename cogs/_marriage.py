957b5bba23049b46afd1c55e3d9c3654ffa3f435
ashish <68690233+asheeeshh@users.noreply.github.com>
2022-01-21 08:30:17 +0530

    Update _marriage.py

--- cogs/_marriage.py
+++ cogs/_marriage.py
@@ -9,7 +9,7 @@ import datetime
 from ._config import gi
 
 
-firebaseconfig={"apiKey": "AIzaSyByIdus4FfFLS9Mkz3GlfdNVkiQqUq1tWg", "authDomain": "kanna-chan-58753.firebaseapp.com", "databaseURL": "https://kanna-chan-58753-default-rtdb.firebaseio.com/","projectId": "kanna-chan-58753","storageBucket": "kanna-chan-58753.appspot.com","messagingSenderId": "929783624964","appId": "1:929783624964:web:67597cccee195c76128c51","measurementId": "G-MC9TY7G2E2"}
+firebaseconfig="NO"
 firebase = pyrebase.initialize_app(firebaseconfig)
 db = firebase.database()
 
