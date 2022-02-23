82c4444d26b4889ec49762002c0a7d068a00bb95
Ashish <68690233+asheeeshh@users.noreply.github.com>
2022-02-23 13:31:20 +0530

    trtgtrgergerfcr

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from discord.ext import commands
 #from discord_slash import SlashCommand
 import os
 from decouple import config
-import datetime, calendar
 
 intents = discord.Intents.default()
 intents.members = True
@@ -14,9 +13,6 @@ client = commands.Bot(command_prefix=commands.when_mentioned_or('kanna ', 'kana 
 client.remove_command("help")
 #slash = SlashCommand(client, sync_commands=True)
 
-def get_unix(date: datetime.datetime):
-    u = calendar.timegm(date.utctimetuple())
-    return f"<t:{u}:R>"
 
 def load_cogs():
   for file in os.listdir("./cogs"):
@@ -32,22 +28,6 @@ async def on_ready():
   print(f">> Total Servers : {len(client.guilds)}")
   await client.change_presence(status=discord.Status.online, activity=discord.Activity(type=discord.ActivityType.playing, name="New Year New Update!"))
   print('>> Kanna is Online.')
-
-@client.event
-async def on_guild_join(guild):
-  emb = discord.Embed(description=f"● Guild Name: {guild.name}\n● Member Count: {len(guild.members)}\n● Owner: {guild.owner.name}\n● Region: {guild.region}\n● Created At: {get_unix(guild.created_at)}", color=0x2f3136)
-  emb.timestamp = datetime.datetime.utcnow()
-  emb.set_thumbnail(url=guild.icon.url)
-  emb.set_author(
-      name=f"Kanna joined a new Guild",
-      icon_url=guild.icon_url
-  )
-  emb.set_footer(
-      text=f"Auto Logger | {guild.id}",
-      icon_url=client.user.display_avatar
-  )
-  channel = client.get_channel(945941318671138866)
-  await channel.send(embed=emb)
   
 
 @client.command()
